4ce2f7d174881f4568ffbd4cdd6c77023ef48f1d
JiYoon Lee <wldbseja@daum.net>
2022-12-12 18:55:06 +0900

    feat (Lotto) : 당첨 번호 중복 확인 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,11 +11,19 @@ class Lotto {
 
   validateWinningNumbers(winningNumbers) {
     this.validateNumbersLength(winningNumbers);
+    this.validateNumbersDuple(winningNumbers);
   }
 
   validateNumbersLength(winningNumbers) {
     if (winningNumbers.length !== 6) {
       throw Error(PRINT_ERROR_STRING.ERROR_NUMBER_LENGTH);
+    }
+  }
+
+  validateNumbersDuple(winningNumbers) {
+    let setWinning = new Set(winningNumbers);
+    if (setWinning.size !== 6) {
+      throw Error(PRINT_ERROR_STRING.ERROR_NUMBER_DUPLE);
     }
   }
 

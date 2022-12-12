789f1040baf7386b727bf6a3d912c20293d9cdbb
JiYoon Lee <wldbseja@daum.net>
2022-12-12 17:44:58 +0900

    test (GameTest) : checkBuyAmount 테스트

---
+++ __tests__/GameTest.js
@@ -0,0 +1,14 @@
+const App = require('../src/App');
+
+describe('로또 게임 기능 테스트', () => {
+  test('checkBuyAmount', () => {
+    const app = new App();
+    expect(() => app.checkBuyAmount(1111)).toThrow();
+    expect(() => app.checkBuyAmount(11111)).toThrow();
+    expect(() => app.checkBuyAmount(111111)).toThrow();
+
+    expect(() => app.checkBuyAmount(1000)).not.toThrow();
+    expect(() => app.checkBuyAmount(10000)).not.toThrow();
+    expect(() => app.checkBuyAmount(100000)).not.toThrow();
+  });
+});

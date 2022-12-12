4427d10a23ee936589745234542703b0ecf1440d
JiYoon Lee <wldbseja@daum.net>
2022-12-12 17:15:47 +0900

    feat (constants) : 에러 문구 추가

--- src/constants.js
+++ src/constants.js
@@ -8,4 +8,11 @@ const PRINT_STRING = {
   WINNING_PHRASE: '\n당첨 통계\n---',
 };
 
-module.exports = { LOTTO_ONE_TICKET, PRINT_STRING };
+const PRINT_ERROR_STRING = {
+  ERROR_BUY_AMOUNT: '[ERROR] 구입 금액은 천 원 단위로 입력해야 합니다. ',
+  ERROR_NUMBER_RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+  ERROR_NUMBER_DUPLE: '[ERROR] 로또 번호가 중복 되었습니다.',
+  ERROR_NUMBER_LENGTH: '[ERROR] 로또 번호의 길이가 맞지 않습니다.',
+};
+
+module.exports = { LOTTO_ONE_TICKET, PRINT_STRING, PRINT_ERROR_STRING };

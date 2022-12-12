1ee4780514f5a8a35467f0bff800e6a96a022dba
JiYoon Lee <wldbseja@daum.net>
2022-12-12 17:15:02 +0900

    feat (constants) : 출력 문구 추가

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,11 @@
 const LOTTO_ONE_TICKET = 1000;
 
-module.exports = { LOTTO_ONE_TICKET };
+const PRINT_STRING = {
+  INPUT_BUY_AMOUNT: '구입금액을 입력해 주세요.\n',
+  BUY_LOTTO_COUNT: (buyLottoCount) => `${buyLottoCount}개를 구매했습니다.`,
+  INPUT_WINNING_NUMBER: '\n당첨 번호를 입력해 주세요.\n',
+  INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
+  WINNING_PHRASE: '\n당첨 통계\n---',
+};
+
+module.exports = { LOTTO_ONE_TICKET, PRINT_STRING };

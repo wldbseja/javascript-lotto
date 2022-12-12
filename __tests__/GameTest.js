94539291d5dace83934a591d10c5e570b0ecec46
JiYoon Lee <wldbseja@daum.net>
2022-12-12 18:49:44 +0900

    test (GameTest) : validateNumbersLength 테스트

--- __tests__/GameTest.js
+++ __tests__/GameTest.js
@@ -1,4 +1,5 @@
 const App = require('../src/App');
+const Lotto = require('../src/Lotto');
 
 describe('로또 게임 기능 테스트', () => {
   test('checkBuyAmount', () => {
@@ -27,5 +28,13 @@ describe('로또 게임 기능 테스트', () => {
     expect(app.convertWinningNumber('6, 5, 4, 3, 2, 1')).toEqual([
       6, 5, 4, 3, 2, 1,
     ]);
+  });
+
+  test('validateNumbersLength', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(() => lotto.validateNumbersLength([1, 2, 3, 4, 5, 6, 7])).toThrow();
+
+    expect(() => lotto.validateNumbersLength([1, 2, 3, 4, 5, 6])).not.toThrow();
   });
 });

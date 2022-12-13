864eef47d44e343bd593053437365d9a557ecb1c
JiYoon Lee <wldbseja@daum.net>
2022-12-13 14:56:59 +0900

    feat (App) : App 처음 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const {
+  LOTTO_ONE_TICKET,
+  PRINT_STRING,
+  PRINT_ERROR_STRING,
+} = require('./constants');
+
 class App {
+  constructor() {}
+
   play() {}
 }
 
+const app = new App();
+app.play();
 module.exports = App;

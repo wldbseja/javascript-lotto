e5a68d0c2d4e699feda61aa7db728a5ccdadaec9
JiYoon Lee <wldbseja@daum.net>
2022-12-13 15:00:35 +0900

    feat (App) : 구입 금액 입출력 , 금액에 따라 메서드 이동 기능

--- src/App.js
+++ src/App.js
@@ -8,7 +8,16 @@ const {
 class App {
   constructor() {}
 
-  play() {}
+  inputBuyAmount() {
+    MissionUtils.Console.readLine(
+      PRINT_STRING.INPUT_BUY_AMOUNT,
+      (buyAmount) => {}
+    );
+  }
+
+  play() {
+    this.inputBuyAmount();
+  }
 }
 
 const app = new App();

37ee74d3b951c3252eac282b58d25e8e5be28cb4
JiYoon Lee <wldbseja@daum.net>
2022-12-14 15:04:56 +0900

    feat (Lotto) : 로또 배열 오름차 순으로 정렬

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,12 +3,12 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 

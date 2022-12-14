a7f7da4ec6f8d7052b9b8c1fe9f72a72b40a46e7
JiYoon Lee <wldbseja@daum.net>
2022-12-14 16:49:19 +0900

    feat (Lotto) : 로또 등 수 index 값 App으로 보내는 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -18,6 +18,24 @@ class Lotto {
   PrintLottoArray(lottoArray) {
     MissionUtils.Console.print(lottoArray);
   }
+
+  getRankIndex(winningNumbers, bonusNumber) {
+    let count = 0;
+    for (let i = 0; i < winningNumbers.length; i++) {
+      if (this.#numbers.includes(winningNumbers[i])) count++;
+    }
+    if (count === 6) return 0;
+    if (count === 3) return 4;
+    if (count === 4) return 3;
+    if (count === 5) {
+      if (this.#numbers.includes(bonusNumber)) {
+        return 1;
+      } else {
+        return 2;
+      }
+    }
+    return 5;
+  }
   // TODO: 추가 기능 구현
 }
 
